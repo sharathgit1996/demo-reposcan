33f13da05d143e133d7e758f3e948690e93759d3
sharathgit1996 <160466976+sharathgit1996@users.noreply.github.com>
2024-02-19 12:49:52 +0530

    Add files via upload

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 # sample_code.py
 
-aws_access_key_id = 'AKIABCDEFG1234567890'
+aws_access_key_id = 'AKIABCDEFG1234567770'
 aws_secret_access_key = 'uJwHSrkNFxJzAP0Wh5+EzrOmNZHlCQuCpjD5uRwR'
 
 # Your actual AWS code here
